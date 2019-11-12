c040ed59a62a91764a89ec61f49ae732aa9d9660
Eamon Sharifi <eamon_s@hotmail.co.uk>
2019-11-12 12:33:50 +0000

    refactor HomeScreent to be a stateful component

--- React-Native-MonitAir/Screens/HomeScreen.tsx
+++ React-Native-MonitAir/Screens/HomeScreen.tsx
@@ -2,15 +2,39 @@ import { StyleSheet, Text, View } from "react-native";
 import Header from "../Components/Header";
 import React from "react";
 
-export default function HomeScreen(props) {
-  return (
-    <>
-      <Header navigate={props.navigation.navigate} />
-      <View style={styles.container}>
-        <Text>Welcome to the MonitAir Homepage</Text>
-      </View>
-    </>
-  );
+export interface Props {
+  navigation: any;
+}
+
+interface State {
+  reading1: number;
+  reading2: number;
+  reading3: number;
+  reading4: number;
+}
+
+export default class Hello extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      reading1: 0,
+      reading2: 0,
+      reading3: 0,
+      reading4: 0
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Header navigate={this.props.navigation.navigate} />
+        <View style={styles.container}>
+          <Text>We're at the homepage</Text>
+        </View>
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
