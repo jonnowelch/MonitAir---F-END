4c89419460d5cf98394a04d8075f6661bc2a0e6a
BeckyDipstick <beckycols91@gmail.com>
2019-11-19 16:45:26 +0000

    I've forgotten what this changes

--- React-Native-MonitAir/Screens/HomeScreen.tsx
+++ React-Native-MonitAir/Screens/HomeScreen.tsx
@@ -89,7 +89,7 @@ export default class HomeScreen extends React.Component<HomeProps, State> {
     return (
       <>
         <Header navigate={this.props.navigation.navigate} />
-        <Text style={{ color: "#13D0FF" }}>
+        <Text style={{ color: "#13D0FF", fontFamily: "sans-serif" }}>
           Hi {loggedInUser[0] && loggedInUser[0].username} welcome to your
           monitAir!
         </Text>
