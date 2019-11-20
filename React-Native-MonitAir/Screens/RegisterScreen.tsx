527b07822767031dfe77fea89a9a8df642dcd290
BeckyDipstick <beckycols91@gmail.com>
2019-11-20 13:34:04 +0000

    removes scrollview

--- React-Native-MonitAir/Screens/RegisterScreen.tsx
+++ React-Native-MonitAir/Screens/RegisterScreen.tsx
@@ -5,8 +5,7 @@ import {
   View,
   TouchableOpacity,
   TextInput,
-  Alert,
-  ScrollView
+  Alert
 } from "react-native";
 import Header from "../Components/Header";
 import firebase from "../firebase.js";
@@ -96,83 +95,81 @@ export default class RegisterScreen extends Component<RegisterProps, State> {
     return (
       <>
         <Header navigate={this.props.navigation.navigate} />
-        <ScrollView>
-          <View>
-            <Text style={{ fontSize: 20, paddingBottom: 20, color: "#13D0FF" }}>
-              Look how far we've come....
-            </Text>
-            <View style={{ paddingBottom: 10 }}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter Forename"
-                value={this.state.first_name}
-                onChangeText={first_name => this.setState({ first_name })}
-              ></TextInput>
-            </View>
-            <View style={{ paddingBottom: 10 }}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter Surname"
-                value={this.state.surname}
-                onChangeText={surname => this.setState({ surname })}
-              ></TextInput>
-            </View>
-            <View style={{ paddingBottom: 10 }}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter Email"
-                value={this.state.email}
-                onChangeText={email => this.setState({ email })}
-              ></TextInput>
-            </View>
-            <View style={{ paddingBottom: 10 }}>
-              <TextInput
-                style={styles.input}
-                placeholder="Create Usernmae"
-                value={this.state.username}
-                onChangeText={username => this.setState({ username })}
-              ></TextInput>
-            </View>
-            <View style={{ paddingBottom: 10 }}>
-              <TextInput
-                style={styles.input}
-                placeholder="Create Password"
-                value={this.state.password}
-                secureTextEntry={true}
-                onChangeText={password => this.setState({ password })}
-              ></TextInput>
-            </View>
-            <View style={{ paddingBottom: 10 }}>
-              <TextInput
-                style={styles.input}
-                placeholder="Enter Sensor ID"
-                value={this.state.sensor_id}
-                onChangeText={sensor_id => this.setState({ sensor_id })}
-              ></TextInput>
-            </View>
-            <View style={{ width: 200, marginTop: 20, alignSelf: "center" }}>
-              <LinearGradient
-                colors={["#3B7BFF", "#13D0FF"]}
-                style={{
-                  padding: 15,
-                  borderRadius: 10,
-                  justifyContent: "center",
-                  alignItems: "center"
+        <View>
+          <Text style={{ fontSize: 20, paddingBottom: 20, color: "#13D0FF" }}>
+            Look how far we've come....
+          </Text>
+          <View style={{ paddingBottom: 10 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Forename"
+              value={this.state.first_name}
+              onChangeText={first_name => this.setState({ first_name })}
+            ></TextInput>
+          </View>
+          <View style={{ paddingBottom: 10 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Surname"
+              value={this.state.surname}
+              onChangeText={surname => this.setState({ surname })}
+            ></TextInput>
+          </View>
+          <View style={{ paddingBottom: 10 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Email"
+              value={this.state.email}
+              onChangeText={email => this.setState({ email })}
+            ></TextInput>
+          </View>
+          <View style={{ paddingBottom: 10 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Create Usernmae"
+              value={this.state.username}
+              onChangeText={username => this.setState({ username })}
+            ></TextInput>
+          </View>
+          <View style={{ paddingBottom: 10 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Create Password"
+              value={this.state.password}
+              secureTextEntry={true}
+              onChangeText={password => this.setState({ password })}
+            ></TextInput>
+          </View>
+          <View style={{ paddingBottom: 10 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Sensor ID"
+              value={this.state.sensor_id}
+              onChangeText={sensor_id => this.setState({ sensor_id })}
+            ></TextInput>
+          </View>
+          <View style={{ width: 200, marginTop: 20, alignSelf: "center" }}>
+            <LinearGradient
+              colors={["#3B7BFF", "#13D0FF"]}
+              style={{
+                padding: 15,
+                borderRadius: 10,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate("Home");
                 }}
               >
-                <TouchableOpacity
-                  onPress={() => {
-                    this.props.navigation.navigate("Home");
-                  }}
-                >
-                  <Text style={{ color: "white", alignSelf: "center" }}>
-                    Submit
-                  </Text>
-                </TouchableOpacity>
-              </LinearGradient>
-            </View>
+                <Text style={{ color: "white", alignSelf: "center" }}>
+                  Submit
+                </Text>
+              </TouchableOpacity>
+            </LinearGradient>
           </View>
-        </ScrollView>
+        </View>
       </>
     );
   }
