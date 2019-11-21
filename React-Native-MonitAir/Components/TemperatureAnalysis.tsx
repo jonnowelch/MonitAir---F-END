efb0f90a841dcf6a8c56fb7a745f9a56665aca1b
Eamon Sharifi <eamon_s@hotmail.co.uk>
2019-11-21 10:27:19 +0000

    Adjustment to text on TempAnalysis comp

--- React-Native-MonitAir/Components/TemperatureAnalysis.tsx
+++ React-Native-MonitAir/Components/TemperatureAnalysis.tsx
@@ -8,18 +8,22 @@ export interface AnalysisProps {
 const TemperatureAnalysis = ({ readings }) => {
   readings = readings.sort((a, b) => a.y - b.y);
 
-  const averageTemp = Math.round(
+  let averageTemp: any =
     readings.map(reading => reading.y).reduce((a, b) => a + b, 0) /
-      readings.length
-  );
+    readings.length;
+
+  averageTemp = averageTemp.toFixed(2);
 
   let rankText: string;
-  if (averageTemp > 21)
+  if (averageTemp > 23)
     rankText =
-      'is too high and can cause cardiovascular risk. You should take measures to keep the overall temperature of your home between 18°C and 21°C';
+      'is too high and can cause cardiovascular risk. You should take measures to keep the overall temperature of your home between 18°C and 21°C.';
+  if (averageTemp <= 23)
+    rankText =
+      'is on the border of being too high. Anything outside of 18°C and 21°C will cause discomfort and extreme temperatures can be damaging to health.';
   if (averageTemp <= 21)
     rankText =
-      'is ideal. Anything outside of 18°C and 21°C will cause discomfort and extreme temperatures can be damaging to health';
+      'is ideal. Try to maintain a healthy temperature of between 18°C and 21°C.';
   if (averageTemp <= 18)
     rankText =
       'is on the border of being too low, which can be damaging to your health. Try and keep your temp between 18°C and 21°C.';
@@ -31,12 +35,12 @@ const TemperatureAnalysis = ({ readings }) => {
       'is far too low and can cause cardiovascular risk. Try and keep your temp between 18°C and 21°C.';
   if (averageTemp <= 9)
     rankText =
-      'is waaaaaayyy too low and can cause a risk of hypothermia. Try and keep your temp between 18°C and 21°C... or you might die.';
+      'is waaaaaayyy too low and can cause a risk of hypothermia. Try and keep your temp between 18°C and 21°C... or you /will/ die.';
 
   return (
     <View>
       <Text>
-        The average temperature of your home on this day was {averageTemp},
+        The average temperature of your home on this day was {averageTemp}°C,
         which {rankText}
       </Text>
     </View>
